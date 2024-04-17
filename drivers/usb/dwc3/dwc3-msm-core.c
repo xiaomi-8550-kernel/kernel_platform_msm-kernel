2f4d79cbbfc830e066bc6c94e7eb66e2b9190914
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-04-17 12:23:09 +0200

    usb: Import Xiaomi changes
     * from fuxi-t-oss
    
    Change-Id: I84cc36106077e55604dd836203065032731d529e

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -6011,6 +6011,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	int ret = 0, i;
 	u32 val;
 
+	pr_info("HI miusb begins %s\n", __func__);
 	mdwc = devm_kzalloc(&pdev->dev, sizeof(*mdwc), GFP_KERNEL);
 	if (!mdwc)
 		return -ENOMEM;
@@ -6238,6 +6239,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 
 	dwc3_msm_get_apsd_source(mdwc);
 
+	pr_info("HI miusb %s get role-switch OK!\n", __func__);
 	if (of_property_read_bool(node, "extcon")) {
 		ret = dwc3_msm_extcon_register(mdwc);
 		if (ret)
@@ -6304,6 +6306,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	if (of_property_read_bool(node, "qcom,msm-probe-core-init"))
 		dwc3_ext_event_notify(mdwc);
 
+	pr_info("HI miusb %s OK!\n", __func__);
 	mdwc->force_disconnect = false;
 	return 0;
 
