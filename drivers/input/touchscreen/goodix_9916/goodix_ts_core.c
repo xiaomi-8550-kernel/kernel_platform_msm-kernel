d64e6665d42ef06dbfa8d32ff887d9d7e4984a04
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-06-06 18:57:35 +0200

    input: touchscreen: goodix_9916: Properly store values of Touch_Aod_Enable and Touch_Doubletap_Mode
    Change-Id: I9091dda7dedab7da219f28434fed315a073afc72

--- drivers/input/touchscreen/goodix_9916/goodix_ts_core.c
+++ drivers/input/touchscreen/goodix_9916/goodix_ts_core.c
@@ -2952,6 +2952,8 @@ static int goodix_set_cur_value(int gtp_mode, int gtp_value)
 	}
 	if (gtp_mode == Touch_Doubletap_Mode && goodix_core_data &&
 	    gtp_value >= 0) {
+		xiaomi_touch_interfaces.touch_mode[gtp_mode][SET_CUR_VALUE] = gtp_value;
+		xiaomi_touch_interfaces.touch_mode[gtp_mode][GET_CUR_VALUE] = gtp_value;
 		goodix_core_data->double_wakeup = gtp_value;
 		queue_work(goodix_core_data->gesture_wq,
 			   &goodix_core_data->gesture_work);
@@ -2959,6 +2961,8 @@ static int goodix_set_cur_value(int gtp_mode, int gtp_value)
 	}
 	if (gtp_mode == Touch_Aod_Enable && goodix_core_data &&
 	    gtp_value >= 0) {
+		xiaomi_touch_interfaces.touch_mode[gtp_mode][SET_CUR_VALUE] = gtp_value;
+		xiaomi_touch_interfaces.touch_mode[gtp_mode][GET_CUR_VALUE] = gtp_value;
 		goodix_core_data->aod_status = gtp_value;
 		queue_work(goodix_core_data->gesture_wq,
 			   &goodix_core_data->gesture_work);
